67fc3f130d15fb7008f5ad074de5c5bdd217487f
BasilKhan07 <basilkhan06@gmail.com>
2024-05-26 13:57:07 +0500

    Course

---
+++ Computer Organization and Assembly Language Theory/Coal Theory/Coal Theory/Text.asm
@@ -0,0 +1,15 @@
+INCLUDE Irvine32.inc
+INCLUDE Macros.inc
+
+BubbleSort PROTO, array1: PTR DWORD, count: DWORD, Key: DWORD
+
+.data
+	array DWORD 1, 4, 75, 3, 7, 24, 8, 4, 0, 28
+	num DWORD ?
+	val DWORD ?
+.code
+main PROC
+	
+exit
+main ENDP
+END main
